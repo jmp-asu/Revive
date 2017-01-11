8d0ebbdd8e10a1c7c3a5ad03f93ae228ef6ea36f
jmp-asu <asu@jmpelletier.com>
2017-01-11 10:16:52 +0900

    間違っているパスを修正

--- main2.js
+++ main2.js
@@ -4,14 +4,14 @@ window.onload = function() {
 	
     var game = new Game(800, 640); 
     game.fps = 15;                 
-    game.preload("images/start2.png",
-				"images/start1.png",
-				"images/radio.png",
-				"images/huki.png",
-				"images/radio2.png",
-				"images/staff.png",
-				"images/map.png",
-				"images/waku.png"
+    game.preload("img/start2.png",
+				"img/start1.png",
+				"img/radio.png",
+				"img/huki.png",
+				"img/radio2.png",
+				"img/staff.png",
+				"img/map.png",
+				"img/waku.png"
 				); 
 				
 	game.preload("sound/title.mp3",
@@ -43,11 +43,11 @@ window.onload = function() {
 	   
 
 			var titleImage = new Sprite(800, 640);                   
-			titleImage.image = game.assets['images/start1.png'];     
+			titleImage.image = game.assets['img/start1.png'];     
 			scene.addChild(titleImage);                                         
             
 			var startImage = new Sprite(146, 58);                   
-			startImage.image = game.assets['images/start2.png'];     
+			startImage.image = game.assets['img/start2.png'];     
 			startImage.x = 325;                                      
 			startImage.y = 500;
 
@@ -75,19 +75,19 @@ window.onload = function() {
 			var scene = new Scene();
 			
 			var img1 = new Sprite(800,640); 
-			img1.image = game.assets["images/radio.png"];
+			img1.image = game.assets["img/radio.png"];
 			img1.x = 0;
 			scene.addChild(img1);
 			
 			var img2 = new Sprite(452,332); 
-			img2.image = game.assets["images/radio2.png"];
+			img2.image = game.assets["img/radio2.png"];
 			img2.x = 170;
 			img2.y = 270;
 			img2.scale(0.5,0.5);
 			scene.addChild(img2);
 			
 			var img3 = new Sprite(200,200); 
-			img3.image = game.assets["images/huki.png"];
+			img3.image = game.assets["img/huki.png"];
 			img3.x = 170;
 			img3.y = 180;
 			img3.scale(1.8,1.5)
@@ -121,7 +121,7 @@ window.onload = function() {
 			var scene = new Scene();
 			           
 			var staff = new Sprite(800, 640);
-			staff.image = game.assets["images/staff.png"];
+			staff.image = game.assets["img/staff.png"];
 			scene.addChild(staff);
 			
 			scene.addEventListener(Event.TOUCH_START, function(e) {
@@ -137,11 +137,11 @@ window.onload = function() {
 			var scene = new Scene();
 			           
 			var map = new Sprite(640,640);
-			map.image = game.assets["images/map.png"];
+			map.image = game.assets["img/map.png"];
 			scene.addChild(map);
 			
 			var waku = new Sprite(160,640);
-			waku.image = game.assets["images/waku.png"];
+			waku.image = game.assets["img/waku.png"];
 			waku.x = 640;
 			waku.y = 0;
 			scene.addChild(waku);
